862a1d0bfef3ca0c4cd225586004e6d72aa463b0
zhanbai <bai.zhan@qq.com>
2022-07-09 13:25:31 +0800

    feat:admin user

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UsersController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'User';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('ID'));
+        $grid->column('name', __('User Name'));
+        $grid->column('phone', __('Phone'));
+        $grid->column('avatar', __('Avatar'))->image(config('app.url'));
+        $grid->column('created_at', __('Created at'));
+
+        // 不在页面显示 `新建` 按钮，因为我们不需要在后台新建用户
+        $grid->disableCreateButton();
+        // 同时在每一行也不显示操作按钮
+        $grid->disableActions();
+
+        return $grid;
+    }
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,7 +10,6 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
     'as'            => config('admin.route.prefix') . '.',
 ], function (Router $router) {
-
     $router->get('/', 'HomeController@index')->name('home');
-
+    $router->get('users', 'UsersController@index');
 });
